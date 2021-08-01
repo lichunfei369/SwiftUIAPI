c74e06efb37f9489f098fb7cd8b3f23ce00577c6
lichunfei <lichunfeigood@gmail.com>
2021-08-01 22:01:10 +0800

    Initial Commit

---
+++ swiftUIAPICalls/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUIAPICalls
+//
+//  Created by lichunfei on 2021/8/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUIAPICalls/swiftUIAPICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIAPICallsApp.swift
+//  swiftUIAPICalls
+//
+//  Created by lichunfei on 2021/8/1.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIAPICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
